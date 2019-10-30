05c1bbb76e6c01afa2ecf20f0e1736b469f89273
MattUDCompSci <mattweis@udel.edu>
2019-10-30 19:31:43 -0400

    Final Canvas Analyzer Project Code

--- canvas_analyzer.py
+++ canvas_analyzer.py
@@ -14,6 +14,7 @@ Refer to the instructions on Canvas for more information.
 author: Matthew Weis
 """
 import canvas_requests
+import datetime
 import matplotlib.pyplot as plt
 __version__ = 7
 
@@ -33,60 +34,73 @@ __version__ = 7
 
 
 def print_user_info(user_dict: dict):
+    #Prints out basic information for the user
     print("Name:", user_dict['name'])
     print("Title:", user_dict['title'])
     print("Primary Email:", user_dict['primary_email'])
     print("Bio:", user_dict['bio'])
+
 def filter_available_courses(course_list):
+    #Returns all of the user's available canvas courses
     available_courses = []
     for course in course_list:
         if course['workflow_state'] == "available":
             available_courses.append(course)
     return available_courses
+
 def print_courses(course_list):
+    #Prints all of the users courses
     for course in course_list:
         print(course['id'], ":", course['name'])
+
 def get_course_ids(course_list):
+    #Returns a list containing all of the user's canvas courses' ID's
     course_id = []
     for course in course_list:
         course_id.append(course['id'])
     return course_id
+
 def choose_course(course_ids):
+    #Asks the user for to input a valid course ID and returns the chosen ID
     selected_course_id = int(input("Please enter a valid course id"))
     while selected_course_id not in course_ids:
         selected_course_id = int(input("Please enter a valid course id"))
     for id in course_ids:
         if id == selected_course_id:
             return id
+
 def summarize_points(submissions):
+    #Consumes a list of submission dictionaries and prints out the total amount of points possible, the points obtained,
+    #and the current grade for the course
     possible_point_sum = 0
     submission_points = 0
     for assignment in submissions:
         if assignment['score'] != None:
-            possible_point_sum = possible_point_sum + (assignment['assignment']['points_possible']*assignment['assignment']['group']['group_weight'])
+            possible_point_sum = possible_point_sum + (assignment['assignment']['points_possible']*
+                                                       assignment['assignment']['group']['group_weight'])
             submission_points = submission_points + (assignment['score']*assignment['assignment']['group']['group_weight'])
     print("Points Possible so far:", possible_point_sum)
     print("Points Obtained", submission_points)
     print("Current Grade:", round(100*(submission_points/possible_point_sum)))
+
 def summarize_groups(submissions):
-    name_list = []
-    total_score = []
-    running_score = 0
+    #Consumes a List of submission dictionaries and prints out the name and unweighted grade for each group
+    counts = {}
+    possible = {}
     running_weight = 0
-    marker = 0
     for submission in submissions:
-        if submission['assignment']['group']['name'] not in name_list:
-            name_list.append(submission['assignment']['group']['name'])
-    for submission in submissions:
-        for group in name_list:
-            if submission['assignment']['group']['name'] == group and submission['score'] != None:
-                running_score = running_score + submission['score']
-                running_weight = running_weight + submission['assignment']['group']['group_weight']
-        total_score.append(100*running_score/running_weight)
-    for group in name_list:
-        print(group, ":", total_score[marker])
-        marker = marker + 1
+        if submission['score'] != None:
+            if submission['assignment']['group']['name'] not in counts:
+                counts[submission['assignment']['group']['name']] = submission['score']
+                possible[submission['assignment']['group']['name']] = submission['assignment']['points_possible']
+            else:
+                counts[submission['assignment']['group']['name']] = counts[submission['assignment']['group']['name']] + submission['score']
+                possible[submission['assignment']['group']['name']] = possible[submission['assignment']['group']['name']] + submission['assignment']['points_possible']
+    for key in counts:
+        print(key, ":", round(100*counts[key]/possible[key]))
+
 def plot_scores(submissions):
+    #Consumes a list of submission dictionaries and plots each submission grade on a bar graph
     scores = []
     for submission in submissions:
         if submission['score'] != None and submission['assignment']['points_possible'] != 0:
@@ -96,7 +110,10 @@ def plot_scores(submissions):
     plt.xlabel("Grades")
     plt.ylabel("Number of Assignments")
     plt.show()
+
 def plot_grade_trends(submissions):
+    #Consumes a list of Submission dictionaries and plots the grade trend of the submissions as a line plot
+    print(submissions)
     max_points = 0
     max_points_list = []
     running_max_points_list = []
@@ -111,6 +128,7 @@ def plot_grade_trends(submissions):
     high_points_list = []
     running_high_points_list = []
     final_high_points_list = []
+    submissions_times = []
     running_sum_high = 0
     max_score = 0
     for submission in submissions:
@@ -143,14 +161,19 @@ def plot_grade_trends(submissions):
         final_low_points_list.append(point / max_score)
     for point in running_high_points_list:
         final_high_points_list.append(point / max_score)
-    plt.plot(final_high_points_list, label="High")
-    plt.plot(final_low_points_list, label="Low")
-    plt.plot(final_max_points_list, label="Max")
-    plt.title("Grade Trends")
+    for submission in submissions:
+        submission_time = datetime.datetime.strptime(submission['assignment']['due_at'], "%Y-%m-%dT%H:%M:%SZ")
+        submissions_times.append(submission_time)
+    plt.plot(submissions_times, final_high_points_list, label="Highest")
+    plt.plot(submissions_times, final_low_points_list, label="Lowest")
+    plt.plot(submissions_times, final_max_points_list, label="Maximum")
+    plt.title("Grade Trend")
     plt.ylabel("Grade")
     plt.legend()
     plt.show()
+
 def main(name):
+    #The main function, executes all the other functions
     print_user_info(canvas_requests.get_user(name))
     print_courses(canvas_requests.get_courses(name))
     courses = filter_available_courses(canvas_requests.get_courses(name))
